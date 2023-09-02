942aa719787057c73c3107f11532bd13e9daeb15
skh <s.koehnhaskins@gmail.com>
2023-09-02 13:53:29 +0200

    updated features: Exit code '1' returned in case of invalid arguments Name of font printed in case of random font selection

--- figlet/README.md
+++ figlet/README.md
@@ -8,3 +8,15 @@
 This program converts any input text to ascii text using the figlet python module. It was developed using Python 3.11.
 
 The program can be run from the command line without flags, in which case a random font will assigned. in order to call a specific font name, add the arguments: -f <"font_name"> OR --font <"font_name">.
+
+If the program is run with no arguments and a random font is assigned, the name of the font will be printed along with the output.
+
+# Use case
+$python3 figlet.py -f <"font_name">
+OR
+$python3 figlet.py
+
+Input: <"text to be formatted">
+
+# Invalid usage
+In case the user enters invalid command line arguments or a non-existent font, an error message is printed and the program returns exit code '1'.
--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,18 +9,19 @@ import sys
 from pyfiglet import Figlet, FigletFont
 from random import randint
 
+
 # Check for correct usage (e.g., python figlet.py || python figlet.py -f <fontname> || python figlet.py --<fontname>)
 args = len(sys.argv) - 1 
 # Check for correct number of arguments
 if args != 0 and args != 2: 
     print("Invalid Usage")
-    sys.exit()
+    sys.exit(1)
 # print(f"arguments: {args}") # print for debugging
 
 # Check if flag is valid
 elif args != 0 and sys.argv[1] != "-f" and sys.argv[1] != "--font":
     print("Invalid usage")
-    sys.exit()
+    sys.exit(1)
 
 # Assign command line font or assign random font
 # NOTE: FigletFont getFonts method returns font names as strings in a list
@@ -35,8 +36,7 @@ else:
 # Validate font choice
 if not font in font_list:
     print("Invalid font choice")
-    sys.exit()
-
+    sys.exit(1)
 
 # Prints input in selected font
 f = Figlet(font=font)
