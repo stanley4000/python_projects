5e58e646ef0621740c57ce220b72a9e477669845
skh <s.koehnhaskins@gmail.com>
2023-09-02 13:06:56 +0200

    Completed v1 of figlet.py Added README.md

---
+++ figlet/README.md
@@ -0,0 +1,10 @@
+#############################
+# Project as assigned in Harvard/edX 2023 CS50:
+# https://cs50.harvard.edu/x/2023/problems/6/figlet/
+# Code uses figlet python module from:
+# https://pypi.org/project/pyfiglet/0.7/
+####################################################
+
+This program converts any input text to ascii text using the figlet python module. It was developed using Python 3.11.
+
+The program can be run from the command line without flags, in which case a random font will assigned. in order to call a specific font name, add the arguments: -f <"font_name"> OR --font <"font_name">.
--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,11 @@
 #############################
-# Project as assigned in CS50:
+# Project as assigned in Harvard/edX 2023 CS50:
 # https://cs50.harvard.edu/x/2023/problems/6/figlet/
 ####################################################
 
 import sys
-from pyFiglet import Figlet
+from pyfiglet import Figlet, FigletFont
+from random import randint
 
 # Check for correct usage (e.g., python figlet.py || python figlet.py -f <fontname> || python figlet.py --<fontname>)
 args = len(sys.argv) - 1 
@@ -12,20 +13,28 @@ args = len(sys.argv) - 1
 if args != 0 and args != 2: 
     print("Invalid Usage")
     sys.exit()
-# print(f"arguments: {args}") #print for debugging
+# print(f"arguments: {args}") # print for debugging
 
 # Check if flag is valid
 elif args != 0 and sys.argv[1] != "-f" and sys.argv[1] != "--font":
     print("Invalid usage")
     sys.exit()
 
-# TODO: validate font choice
-# Assign font
+# Assign command line font or assign random font
+# NOTE: FigletFont getFonts method returns font names as strings in a list
+font_list = FigletFont.getFonts()
 if args > 0:
     font = sys.argv[2]
-# TODO: choose random font
 else:
-    font = 'slant'
+    index = randint(1, len(FigletFont.getFonts()))
+    font = font_list[index]
+
+# TODO: validate font choice
+if not font in font_list:
+    print("Invalid font choice")
+    sys.exit()
+
+
 # print(f"font = {font}") #print for debugging
 
 # Prints input in selected font
